2be4fb6c96a6d4acc508f368d8db5be19deabce5
Darin Josell <darin.j.manley@gmail.com>
2025-10-24 16:46:50 -0400

    Updated main.py

--- fastapi-backend/main.py
+++ fastapi-backend/main.py
@@ -205,7 +205,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "main:app",
         host="0.0.0.0",
-        port=8000,
+        port=8663,
         reload=True,
         log_level="info"
     )
